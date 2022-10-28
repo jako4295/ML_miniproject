c0b2b76c62623277b5d943651725964bbfefdf3a
Anders <ahll19@student.aau.dk>
2022-10-28 09:48:49 +0200

    tmp commit

--- methods_NN.py
+++ methods_NN.py
@@ -1,5 +1,8 @@
 from torch.utils.data import Dataset
 import torch
+import numpy as np
+import pandas as pd
+from torch import nn
 
 
 class NumbersDataset(Dataset):
@@ -12,3 +15,54 @@ class NumbersDataset(Dataset):
 
     def __getitem__(self, idx):
         return self.samples[idx], self.labels[idx]
+
+
+def load_data(dim_type: str) -> list[np.array]:
+    match dim_type.lower():
+        case "full":
+            file_names = [
+                "data/trn_all.csv",
+                "data/tst_all.csv",
+            ]
+        case "pca2":
+            file_names = [
+                "data/trn_pca2.csv",
+                "data/tst_pca2.csv",
+            ]
+        case "pca10":
+            file_names = [
+                "data/trn_pca10.csv",
+                "data/tst_pca10.csv",
+            ]
+        case other:
+            raise KeyError("dim_type must be: 'full', 'pca2', or 'pca10'")
+    
+    file_names += ["data/trn_labs.csv", "data/tst_labs.csv"]
+    
+    return (pd.read_csv(f).to_numpy() for f in file_names)
+
+
+class Network(nn.Module):
+    def __init__(self, hidden_layer_dims: list[int], input_size: int, device: torch.device) -> None:
+        super(Network, self).__init__()
+        self.device = device
+        
+        self.softmax = nn.Softmax(dim=2).to(device)
+        self.relu = nn.ReLU().to(device)
+        self.l1 = nn.Linear(input_size, hidden_layer_dims[0]).to(device)
+        self.ln = nn.Linear(hidden_layer_dims[-1], 2).to(device)
+        self.ls = [self.l1]
+        
+        for i in range(len(hidden_layer_dims) - 1):
+            self.ls.append(
+                nn.Linear(hidden_layer_dims[i], hidden_layer_dims[i+1]).to(device)
+            )
+        self.ls.append(self.ln)
+        
+    def forward(self, x: np.array) -> np.array:
+        out = self.relu(self.ls[0](x))
+        for l in self.ls[1:-1]:
+            out = self.relu(l(out))
+        out = self.ls[-1](out)
+        
+        return out
